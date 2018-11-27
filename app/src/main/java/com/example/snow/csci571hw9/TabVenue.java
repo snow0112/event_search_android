9fa1218f3031e605d5f25eda38bd314f0e741274
snow0112 <hsuehweh@usc.edu>
2018-11-27 10:49:50 -0800

    venue without google map

--- app/src/main/java/com/example/snow/csci571hw9/TabVenue.java
+++ app/src/main/java/com/example/snow/csci571hw9/TabVenue.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -24,6 +25,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
@@ -31,6 +33,10 @@ import java.net.URLEncoder;
 
 public class TabVenue extends Fragment implements OnMapReadyCallback {
     private  String venuename;
+    private TextView name, address, city, phone, hours, rule, childrule;
+    private TableRow row_name, row_address, row_city, row_phone, row_hours, row_rule, row_childrule;
+    private JSONObject VENUE = new JSONObject();
+    private String lat, lon;
 
 
     public TabVenue() {
@@ -56,7 +62,20 @@ public class TabVenue extends Fragment implements OnMapReadyCallback {
                 (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
+                        try {
+                            VENUE = response.getJSONObject("_embedded").getJSONArray("venues").getJSONObject(0);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        setName(VENUE);
+                        setAddress(VENUE);
+                        setCity(VENUE);
+                        setPhone(VENUE);
+                        setHours(VENUE);
+                        setRule(VENUE);
+                        setChildrule(VENUE);
 
+                        setlocation(VENUE);
                     }
                 }, new Response.ErrorListener() {
                     @Override
@@ -86,6 +105,136 @@ public class TabVenue extends Fragment implements OnMapReadyCallback {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        this.name = getView().findViewById(R.id.venue_name);
+        address = getView().findViewById(R.id.venue_address);
+        city = getView().findViewById(R.id.venue_city);
+        phone = getView().findViewById(R.id.venue_phone);
+        hours = getView().findViewById(R.id.venue_hours);
+        rule = getView().findViewById(R.id.venue_rule);
+        childrule = getView().findViewById(R.id.venue_child);
+
+        this.row_name = getView().findViewById(R.id.row_venue_name);
+        row_address = getView().findViewById(R.id.row_venue_address);
+        row_city = getView().findViewById(R.id.row_venue_city);
+        row_phone = getView().findViewById(R.id.row_venue_phone);
+        row_hours = getView().findViewById(R.id.row_venue_hours);
+        row_rule = getView().findViewById(R.id.row_venue_rule);
+        row_childrule = getView().findViewById(R.id.row_venue_child);
+
+        setName(VENUE);
+        setAddress(VENUE);
+        setCity(VENUE);
+        setPhone(VENUE);
+        setHours(VENUE);
+        setRule(VENUE);
+        setChildrule(VENUE);
+
+        setlocation(VENUE);
+
+    }
+
+    private void setName(JSONObject VENUE){
+        try {
+            String temp = VENUE.getString("name");
+            name.setText(temp);
+            row_name.setVisibility(View.VISIBLE);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            row_name.setVisibility(View.GONE);
+        }
+    }
+    private void setAddress(JSONObject VENUE){
+        try {
+            String temp = VENUE.getJSONObject("address").getString("line1");
+            address.setText(temp);
+            row_address.setVisibility(View.VISIBLE);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            row_address.setVisibility(View.GONE);
+        }
+    }
+    private void setCity(JSONObject VENUE){
+
+        Boolean citydataexist = false;
+        Boolean statedataexist = false;
+        String temp_city = new String(), temp_state = new String();
+        try {
+            temp_city = VENUE.getJSONObject("city").getString("name");
+            citydataexist = true;
+        } catch (JSONException e){ }
+        try {
+            temp_state = VENUE.getJSONObject("state").getString("name");
+            statedataexist = true;
+        } catch (JSONException e){ }
+        String temp = new String();
+        if (citydataexist && statedataexist){
+            temp = temp_city + ", "+ temp_state;
+            row_city.setVisibility(View.VISIBLE);
+        }
+        if (citydataexist && !statedataexist){
+            temp = temp_city;
+            row_city.setVisibility(View.VISIBLE);
+        }
+        if (!citydataexist && statedataexist){
+            temp = temp_state;
+            row_city.setVisibility(View.VISIBLE);
+        }
+        if (!citydataexist && !statedataexist){
+            row_city.setVisibility(View.GONE);
+        }
+        city.setText(temp);
+    }
+    private void setPhone(JSONObject VENUE){
+        try {
+            String temp = VENUE.getJSONObject("boxOfficeInfo").getString("phoneNumberDetail");
+            phone.setText(temp);
+            row_phone.setVisibility(View.VISIBLE);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            row_phone.setVisibility(View.GONE);
+        }
+    }
+    private void setHours(JSONObject VENUE){
+        try {
+            String temp = VENUE.getJSONObject("boxOfficeInfo").getString("openHoursDetail");
+            hours.setText(temp);
+            row_hours.setVisibility(View.VISIBLE);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            row_hours.setVisibility(View.GONE);
+        }
+    }
+    private void setRule(JSONObject VENUE){
+        try {
+            String temp = VENUE.getJSONObject("generalInfo").getString("generalRule");
+            rule.setText(temp);
+            row_rule.setVisibility(View.VISIBLE);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            row_rule.setVisibility(View.GONE);
+        }
+    }
+    private void setChildrule(JSONObject VENUE){
+        try {
+            String temp = VENUE.getJSONObject("generalInfo").getString("childRule");
+            childrule.setText(temp);
+            row_childrule.setVisibility(View.VISIBLE);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            row_childrule.setVisibility(View.GONE);
+        }
+    }
+
+    private void setlocation(JSONObject VENUE){
+        try {
+            String lat = VENUE.getJSONObject("location").getString("latitude");
+            String lon = VENUE.getJSONObject("location").getString("longitude");
+            String temp = "lat = " + lat + ", lon = " + lon;
+            TextView location = getView().findViewById(R.id.venue_location);
+            location.setText(temp);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
